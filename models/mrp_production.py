ed0cc0e1438308a4fae9b9e4c6a5c791a679491a
pmorettobr <pmorettobr@gmail.com>
2026-02-17 16:39:38 -0300

    Update mrp_production.py

--- models/mrp_production.py
+++ models/mrp_production.py
@@ -4,8 +4,18 @@ from odoo import models, fields
 class MrpProduction(models.Model):
     _inherit = 'mrp.production'
 
-    equipment_entry_id = fields.Many2one('equipment.entry', string='Entrada de Equipamento', copy=False, readonly=True)
+    equipment_entry_id = fields.Many2one(
+        'equipment.entry', 
+        string='Entrada de Equipamento', 
+        copy=False, 
+        readonly=True
+    )
     
     # Community: Campos para rastreio manual
-    current_workcenter_id = fields.Many2one('mrp.workcenter', string='Máquina Atual')
-    current_operation = fields.Char(string='Operação Atual')
+    current_workcenter_id = fields.Many2one(
+        'mrp.workcenter', 
+        string='Máquina Atual'
+    )
+    current_operation = fields.Char(
+        string='Operação Atual'
+    )
